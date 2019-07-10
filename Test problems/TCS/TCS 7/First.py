c1573c44dc80b8bdef0e6687239b0cef7a3004b1
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-07-10 21:50:29 +0530

    Update First.py

--- Test problems/TCS/TCS 7/First.py
+++ Test problems/TCS/TCS 7/First.py
@@ -10,3 +10,19 @@ for i in range(n+1):
 else:
     print("No")
     
+---------------------------------------------------------------------    
+    
+n=list(map(int,input().split()))
+v=len(n)
+c=0
+for i in range(v+1):
+    if(n[i]>=n[v-1]):
+        c+=n[i]
+        b=bin(c^n[i])
+        print("Yes")
+        break
+    else:
+        print("No")
+        break
+    
+    
