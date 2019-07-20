096def2a70b1c2f84949cdd16eabcce26caec930
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-07-20 12:11:29 +0530

    Update count.py

--- Test problems/TCS/TCS 16/count.py
+++ Test problems/TCS/TCS 16/count.py
@@ -4,3 +4,14 @@ for i in range(1,n+1):
     k=str(i)
     m+=k.count('1')
 print(m)
+
+
+Sample Input 0
+
+13
+Sample Output 0
+
+6
+Explanation 0
+
+Total number of 1's from 0 to 13 is 6. The occurence of 1 are in the numbers 1,10,11,12,13
