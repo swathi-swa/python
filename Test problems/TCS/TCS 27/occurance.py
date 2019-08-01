0c31e1cb258335408fa44f521327040e4e3947d0
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-08-01 21:50:42 +0530

    Create occurance.py

---
+++ Test problems/TCS/TCS 27/occurance.py
@@ -0,0 +1,19 @@
+n=int(input())
+s=''
+for i in range(n+1):
+    s+=str(i)
+print(s.count('1'))
+
+
+
+INPUT:
+
+13
+
+OUTPUT:
+
+6
+
+EXPLANATION:
+
+Total number of 1's from 0 to 13 is 6. The occurence of 1 are in the numbers 1,10,11,12,13
